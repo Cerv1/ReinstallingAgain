5c744725232f521c091b70d577eb1c296f27af2b
Cerv1 <seryi_111@hotmail.es>
2016-11-14 12:23:47 +0100

    Added Dropbox's installation.

--- install.sh
+++ install.sh
@@ -28,7 +28,15 @@ echo "________________________________"
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo wget https://atom.io/download/deb
 
+
+# GITHUB PART: If some repositories are needed, in this section we'll add it.
+git clone https://github.com/zant95/elementary-dropbox /tmp/elementary-dropbox
+bash /tmp/elementary-dropbox/install.sh
+
+
 sudo apt update
+
+# gdebi makes everything simpler because it resolves the dependencies.
 sudo apt install gdebi
 
 sudo gdebi google-chrome-stable_current_amd64.deb
@@ -36,6 +44,3 @@ sudo gdebi atom-amd64.deb
 
 sudo rm google-chrome-stable_current_amd64.deb
 sudo rm atom-amd64.deb
-
-
-
